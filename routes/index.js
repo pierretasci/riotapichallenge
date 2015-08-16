50d4e8c3a89c533d9bbdc33451f42a81c20e4ae6
Pierre Tasci <ptasci67@gmail.com>
2015-08-16 12:59:30 -0700

    Working image on screen

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,11 @@ var RiotDAO = require('../dao/RiotDAO');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
- 
-
   RiotDAO.getChampions().then(function(results) {
-    res.render('index', { rawData:  results, title:'Black Market Bureaucrats'});
+    res.render('index', {
+      rawData:  results, 
+      title: "Black Market Bureaucrats"
+    });
   });
 });
 
