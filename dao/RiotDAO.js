51a6caea98b4a224ea457c21f514d0d64fcfd0a9
Pierre Tasci <ptasci67@gmail.com>
2015-08-15 20:04:58 -0700

    Added a working dao

---
+++ dao/RiotDAO.js
@@ -0,0 +1,20 @@
+var rp = require("request-promise");
+var sprintf = require("sprintf-js").sprintf;
+var config = require("config");
+
+// CONSTANTS
+var BASE_ENDPOINT = "https://na.api.pvp.net";
+var API_KEY = config.get("RIOT_API_KEY");
+
+// HELPER METHODS
+var getMatchEndpoint = function(region, matchId) {
+  return BASE_ENDPOINT + 
+    sprintf("/api/lol/%s/v2.2/match/%s", region, matchId) +
+    "?api_key=" + API_KEY;
+}
+
+module.exports = {
+  getMatch: function(matchId) {
+    return rp(getMatchEndpoint("na", matchId));
+  }
+}
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var RiotDAO = require('../dao/RiotDAO');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
