cc9f68d9b92da78c3c3df6f4c48aea76bd8fd3bc
Pierre Tasci <ptasci67@gmail.com>
2015-08-16 10:52:24 -0700

    print to screen

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,10 @@ var RiotDAO = require('../dao/RiotDAO');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+ 
   RiotDAO.getMatch("1907157315").then(function(results) {
-    console.log(results);
+    res.render('index', { matchData:  results});
   });
-
-  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
