01845b2f8d6739963abd23872fceca1e3ff4ac22
Pierre Tasci <ptasci67@gmail.com>
2015-08-16 10:50:11 -0700

    Test api

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,10 @@ var RiotDAO = require('../dao/RiotDAO');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  RiotDAO.getMatch("1907157315").then(function(results) {
+    console.log(results);
+  });
+
   res.render('index', { title: 'Express' });
 });
 
