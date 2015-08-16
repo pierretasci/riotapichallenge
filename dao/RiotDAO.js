1af6806dbf1ef6084e7720d55e252e859bd3da5d
Pierre Tasci <ptasci67@gmail.com>
2015-08-16 11:49:18 -0700

    Push data to frontend and also added includeTimeline

--- dao/RiotDAO.js
+++ dao/RiotDAO.js
@@ -7,10 +7,12 @@ var BASE_ENDPOINT = "https://na.api.pvp.net";
 var API_KEY = config.get("RIOT_API_KEY");
 
 // HELPER METHODS
-var getMatchEndpoint = function(region, matchId) {
+var getMatchEndpoint = function(region, matchId, includeTimeline) {
+  includeTimeline = includeTimeline == null ? false : includeTimeline;
   return BASE_ENDPOINT + 
     sprintf("/api/lol/%s/v2.2/match/%s", region, matchId) +
-    "?api_key=" + API_KEY;
+    "?api_key=" + API_KEY + 
+    "&includeTimeline=" + includeTimeline;
 }
 
 module.exports = {
