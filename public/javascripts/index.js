acfadcce3dfda7025977bb8443b31fb591e6b3e4
Armen Ourfalian <aourfalian@gmail.com>
2015-08-16 14:56:38 -0700

    Adding navBar

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -3,7 +3,7 @@ var allChamps = data.data;
 var champClass = document.getElementsByClassName('champSquare');
 
 for (var i=0;i<champClass.length;i++) {
-	champClass[i].addEventListener('mousedown',function(e) {
+	champClass[i].addEventListener('click',function(e) {
 		var champID = e.currentTarget.dataset.id;
 		window.location+=champID;
 	})
