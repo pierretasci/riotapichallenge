705dfb4dfbf6807ef0f371e6e7e3bdce52ce4a7c
Armen Ourfalian <aourfalian@gmail.com>
2015-08-17 02:55:23 -0700

    Search Bar Works Now: Now you can search the name of a champion, and click on it to load the page.
    I will add an event listener for pressing Enter while typing

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -6,22 +6,69 @@ var searchField = document.getElementById('searchField');
 
 searchField.addEventListener('input',function(e) {
   var inp = searchField.value;
-    console.log('hi');
+  searchResults(champSearch(inp));
 })
 
 var champList = allChamps['data'];
 
-var searchChampions = function(phrase) {
+
+// Input: phrase (string)
+// Searches for any champions whose name includes the phrase
+// Returns a list of champion objects that are a match
+var champSearch = function(phrase) {
+  if (phrase.length==0) {return []}
   var matches = [];
   phrase = phrase.toUpperCase();
   phrase = phrase.replace(/\s/g, '');
  
   for(var champion in champList) {
-    var champName = champList[champion].name
+    var champName = champList[champion].name;
+    var champTitle = champList[champion].title;
     champName =  champName.toUpperCase();
     champName =  champName.replace(/\s/g, '');
     champName = champName.replace("'",'');
-    if(champName.search(phrase)!=-1) {matches.push(champList[champion].name)}
+
+    if(champName.search(phrase)!=-1) {
+      matches.push(champList[champion])
+    }
   }
 return matches;
+}
+
+var searchOutput = document.getElementById('searchOutput');
+var ddragon = '<img src="http://ddragon.leagueoflegends.com/cdn/5.15.1/img/champion/';
+
+var searchResults = function(objList) {
+  searchOutput.innerHTML='';  
+  if (objList.length>0) {
+    removeClass(searchOutput,'hidden');
+    for(var i=0;i<objList.length;i++) {
+      // Maybe I should do this with jade
+      searchOutput.innerHTML+='<a href="'+objList[i].id+'"<div class="searchResultChamp">' +
+                                ddragon+objList[i].key+'.png"/>'
+                                + objList[i].name+' '+objList[i].title+'</div></a></br>';
+    }
+  }
+  else { 
+    addClass(searchOutput,'hidden')
+    removeClass(searchOutput,'scrollBar');
+    ;}
+  if(searchOutput.clientHeight>300) {addClass(searchOutput,'scrollBar');}
+  else {removeClass(searchOutput,'scrollBar');}
+}
+
+
+
+
+// GLOBAL HELPER FUNCTIONS
+function addClass(el, cls) {
+  var classes = el.className.split(' ');
+  // if(el.className.search(cls)!=-1){
+    classes.push(cls);
+    el.className = classes.join(' ');
+  // }
+}
+
+function removeClass(el, cls) {
+  el.className = el.className.replace(cls,'');
 }
