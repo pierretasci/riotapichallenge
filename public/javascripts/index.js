a41805454a313531edffaa3972f0b6192a5a3805
Armen Ourfalian <aourfalian@gmail.com>
2015-08-17 00:54:53 -0700

    Champion search function works. Now to use it in a good way

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,27 +1,27 @@
+
 // index.js is linked to more than just the home page, the other pages don't have the data variable
 var champClass = document.getElementsByClassName('champSquare');
-
 // Search Bar
 var searchField = document.getElementById('searchField');
 
 searchField.addEventListener('input',function(e) {
   var inp = searchField.value;
     console.log('hi');
-  
 })
+
+var champList = allChamps['data'];
 
 var searchChampions = function(phrase) {
   var matches = [];
   phrase = phrase.toUpperCase();
   phrase = phrase.replace(/\s/g, '');
-  
-  for (var champName in allChamps) {
-    var name = champName;
-    console.log(allChamps.name);
-    name = name.toUpperCase();
-    if(name.search(phrase)>-1) {
-      matches.push(champName);
-    };
+ 
+  for(var champion in champList) {
+    var champName = champList[champion].name
+    champName =  champName.toUpperCase();
+    champName =  champName.replace(/\s/g, '');
+    champName = champName.replace("'",'');
+    if(champName.search(phrase)!=-1) {matches.push(champList[champion].name)}
   }
-  return matches;
+return matches;
 }
