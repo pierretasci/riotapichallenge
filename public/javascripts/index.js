8b2587e9f82b997a080941dba9b7079fa5dd8b3e
Armen Ourfalian <aourfalian@gmail.com>
2015-08-16 21:57:26 -0700

    Cleaned up redundancies

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,23 +1,27 @@
-
 // index.js is linked to more than just the home page, the other pages don't have the data variable
 var champClass = document.getElementsByClassName('champSquare');
-
-// Add event listeners for all the champion portraits
-if(data) {
-  var allChamps = data.data;
-
-  for (var i=0;i<champClass.length;i++) {
-  	champClass[i].addEventListener('click',function(e) {
-  		var champID = e.currentTarget.dataset.id;
-  		window.location.pathname = champID;
-  	})
-  }
-}
 
 // Search Bar
 var searchField = document.getElementById('searchField');
 
 searchField.addEventListener('input',function(e) {
-  console.log('hi');
-  console.log(searchField.value);
+  var inp = searchField.value;
+    console.log('hi');
+  
 })
+
+var searchChampions = function(phrase) {
+  var matches = [];
+  phrase = phrase.toUpperCase();
+  phrase = phrase.replace(/\s/g, '');
+  
+  for (var champName in allChamps) {
+    var name = champName;
+    console.log(allChamps.name);
+    name = name.toUpperCase();
+    if(name.search(phrase)>-1) {
+      matches.push(champName);
+    };
+  }
+  return matches;
+}
