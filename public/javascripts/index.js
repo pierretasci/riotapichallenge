962b19ff365cff6e9520dd7a478e7760d0325ca8
Armen Ourfalian <aourfalian@gmail.com>
2015-08-16 20:11:30 -0700

    -Added a search Field (no functionality yet) -Added a background image in the champion pages -Added a script in champion.jade to pass the champion data to champion.js -NEED: a page for About so we can explain what we're doing (we'll work on it last, but we should still have the page for functionality purposes)

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,11 +1,26 @@
-var allChamps = data.data;
-
+// index.js is linked to more than just the home page, the other pages don't have the data variable
 var champClass = document.getElementsByClassName('champSquare');
+var navButtons = document.getElementsByClassName('navButton');
 
-for (var i=0;i<champClass.length;i++) {
-	champClass[i].addEventListener('click',function(e) {
-		var champID = e.currentTarget.dataset.id;
-		window.location+=champID;
-	})
+// Add event listeners for all the champion portraits
+if(data) {
+  var allChamps = data.data;
+
+  for (var i=0;i<champClass.length;i++) {
+  	champClass[i].addEventListener('click',function(e) {
+  		var champID = e.currentTarget.dataset.id;
+  		window.location+=champID;
+  	})
+  }
 }
 
+//If you click on an <li> in the navBar, you will go to that page
+for (var i=0;i<navButtons.length;i++) {
+  navButtons[i].addEventListener('click',function(e) {
+    var link = e.currentTarget.firstChild;
+    link.click();
+  }) 
+}
+
+// Search Bar
+var searchField = document.getElementById('searchField');
