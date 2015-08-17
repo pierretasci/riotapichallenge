8f5a556a7707d03605dc91bba8fa08fe2839f2bb
Armen Ourfalian <aourfalian@gmail.com>
2015-08-16 20:43:53 -0700

    -Added homePage.js (only for the home page) as opposed to index.js (for all pages) -Started working on searchBar layout.jade now passes a variable called allChamps to JS

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,20 +1,7 @@
-// index.js is linked to more than just the home page, the other pages don't have the data variable
-var champClass = document.getElementsByClassName('champSquare');
-var navButtons = document.getElementsByClassName('navButton');
-
-// Add event listeners for all the champion portraits
-if(data) {
-  var allChamps = data.data;
-
-  for (var i=0;i<champClass.length;i++) {
-  	champClass[i].addEventListener('click',function(e) {
-  		var champID = e.currentTarget.dataset.id;
-  		window.location+=champID;
-  	})
-  }
-}
+// index.js is linked to all the pages
 
 //If you click on an <li> in the navBar, you will go to that page
+var navButtons = document.getElementsByClassName('navButton');
 for (var i=0;i<navButtons.length;i++) {
   navButtons[i].addEventListener('click',function(e) {
     var link = e.currentTarget.firstChild;
@@ -24,3 +11,7 @@ for (var i=0;i<navButtons.length;i++) {
 
 // Search Bar
 var searchField = document.getElementById('searchField');
+
+searchField.addEventListener('input',function(e) {
+  
+})
