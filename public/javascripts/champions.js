251967a1ff04387c9f8fb1bdc10a3940eb5bdc8c
Armen Ourfalian <aourfalian@gmail.com>
2015-08-16 20:48:27 -0700

    homePage.js
    Here it is

---
+++ public/javascripts/champions.js
@@ -0,0 +1,4 @@
+// var champ has all the champion info
+// champ.name and champ.id will probably be the most useful ones
+document.body.style.backgroundImage="url('http://ddragon.leagueoflegends.com/cdn/img/champion/splash/"+champ.key+"_0.jpg')";
+
