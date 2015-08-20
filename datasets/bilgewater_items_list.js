4abd8b46ff147773caaa84c59ebf838929c0b942
Armen Ourfalian <aourfalian@gmail.com>
2015-08-20 02:13:12 -0700

    Champion Builds and item Purchases
    *Added a list of all the bilgewater items and their id's
    *NEED a list of items (just like allChamps) for the champion pages
    *NEW IDEA: each of the bilgewater items should have their own page (like
    the champions) that shows data on them.
    
    *Major Change(s): Champion Pages now show all the builds and all the
    purchased items. This is NOT the final version of the page, it was just
    an idea i was playing with.

---
+++ datasets/bilgewater_items_list.js
@@ -0,0 +1,16 @@
+var bilgewaterItems = [
+  {"itemId":3150,"name":"Mirage Blade"},
+  {"itemId":3430,"name":"Rite of Ruin"},
+  {"itemId":3431,"name":"Netherstride Grimoire"},
+  {"itemId":3434,"name":"Pox Arcana"},
+  {"itemId":3652,"name":"Typhoon Claws"},
+  {"itemId":3742,"name":"Dead Man's Plate"},
+  {"itemId":3744,"name":"Staff of Flowing Water"},
+  {"itemId":3745,"name":"Puppeteer"},
+  {"itemId":3829,"name":"Trickster's Glass"},
+  {"itemId":3840,"name":"Globe of Trust"},
+  {"itemId":3841,"name":"Swindler's Orb"},
+  {"itemId":3844,"name":"Murksphere"},
+  {"itemId":3911,"name":"Martyr's Gambit"},
+  {"itemId":3924,"name":"Flesheater"}
+]
--- public/javascripts/champions.js
+++ public/javascripts/champions.js
@@ -1,6 +1,9 @@
 // var champ has all the champion info
 // champ.name and champ.id will probably be the most useful ones
 document.body.style.backgroundImage="url('http://ddragon.leagueoflegends.com/cdn/img/champion/splash/"+champ.key+"_0.jpg')";
+var itemImage = function(itemId) {
+  return "http://ddragon.leagueoflegends.com/cdn/5.15.1/img/item/"+itemId+".png" 
+} 
 var numGames = champData.length;
 var wins = 0;
 var boughtItems = [];
@@ -21,6 +24,7 @@ for (var i=0;i<numGames;i++) {
     var itemJ = champData[i].participantInformation.stats['item'+j];
     //save the full build of this game
     builds[i].push(itemJ);
+    boughtItems.push({"itemId":itemJ,"timesBought":1,"timesWon":1*winner});
   }
   // Want to know if this build worked or not
   builds[i].push(winner);
@@ -29,3 +33,65 @@ var winrate = 100*wins/numGames;
 winrate = winrate.toFixed(2);
 document.getElementById('numGames').innerHTML+=' '+numGames;
 document.getElementById('winrate').innerHTML+=' '+winrate+' %';
+
+
+var buildsTableBody = document.getElementById('buildsTableBody');
+var fillBuildsTable = function() {
+  for(var i=0;i<builds.length;i++) {
+    builds[i].sort();
+    var rowData = "";
+    for(var k=0;k<builds[i].length-1;k++) {
+      if(builds[i][k]!=0){
+        rowData+="<td>"+"<img alt='"+builds[i][k]+"' src='"+itemImage(builds[i][k])+"'/></td>";
+      }
+      else {
+        rowData+="<td></td>"
+      }
+    }
+    rowData+="<td>"+builds[i][builds[i].length-1]+"</td>";
+    buildsTableBody.innerHTML+="<tr>"+rowData+"</tr>";
+  }
+}
+
+//Combine all the instances of the same item into one
+for(var i=0;i<boughtItems.length;i++) {
+  var itemI = boughtItems[i];
+  for(var j=i;j<boughtItems.length;j++){
+    if(boughtItems[j]["itemId"]==itemI["itemId"]) {
+      itemI["timesBought"]++;
+      itemI["timesWon"]+=boughtItems[j]["timesWon"];
+      boughtItems.splice(j,1);
+      boughtItems[i]=itemI;
+    }
+  }
+}
+var fillItemTable = function() {
+  for(var i=0;i<boughtItems.length;i++) {
+  var winrate = boughtItems[i]["timesWon"]/boughtItems[i]["timesBought"]*100;
+  var rowData="";
+  if(boughtItems[i]["itemId"]==0) {continue}
+  winrate = winrate.toFixed(2);
+  rowData+="<td>"+"<img alt='"+boughtItems[i]["itemId"]+"' src='"+itemImage(boughtItems[i]["itemId"])+"'/></td>";
+  rowData+="<td>"+boughtItems[i]["itemId"]+"</td>";
+  rowData+="<td>"+boughtItems[i]["timesBought"]+"</td>";
+  rowData+="<td>"+winrate+" %</td>";
+  document.getElementById("itemTableBody").innerHTML+="<tr>"+rowData+"</tr>";
+  }
+}
+
+// compares 2 items based on the key provided
+var sortByTimesBought = function(a,b) {
+  if(a["timesBought"]<b["timesBought"]) {return 1}
+  else if(a["timesBought"]>b["timesBought"]) {return -1}
+  else {return 0} 
+}
+
+boughtItems = boughtItems.sort(sortByTimesBought);
+
+fillBuildsTable();
+fillItemTable();
+
+
+var array = [-1,2,-3,4,-5,-4]
+
+
