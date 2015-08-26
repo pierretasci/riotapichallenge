b0e5be5c239b6d2fb502de04dfe541dd9157eec2
Pierre Tasci <ptasci67@gmail.com>
2015-08-25 23:06:07 -0700

    Fixed silly error

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
-var config = require('config');s
+var config = require('config');
 var bodyParser = require('body-parser');
 var Parse = require('parse').Parse;
 
